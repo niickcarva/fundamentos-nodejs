286b0ad350cf425555344e9668fac3e324a76ea5
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-04 12:36:12 -0300

    using streams on users endpoints

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,28 +2,41 @@ import http from "node:http";
 
 // stateful
 const users = [];
+let userId = 1;
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
   console.log(method, url);
 
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
   if (method === "GET" && url === "/users") {
-    return response
+    return res
       .setHeader("Content-Type", "application/json")
       .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
-      id: 1,
-      name: "John Doe",
-      email: "n7d7V@example.com",
+      id: userId++,
+      name,
+      email,
     });
 
-    return response.writeHead(201).end();
+    return res.writeHead(201).end();
   }
 
-  return response.writeHead(404).end("NOT FOUND");
+  return res.writeHead(404).end("NOT FOUND");
 });
 
 server.listen(3333);
