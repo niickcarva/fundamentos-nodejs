c5419d7680f3f47c9a8e9f9bc75c2a7f7a8f77f4
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-04 12:55:14 -0300

    middleware concept

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 // stateful
 const users = [];
@@ -8,21 +9,10 @@ const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   console.log(method, url);
 
-  const buffers = [];
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
